dfd75b8f8160472f3da2c94979b455d84b0ace68
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2023-01-28 15:32:35 +0300

    add 10TiB solution

---
+++ Memory/solution/10TiB.c
@@ -0,0 +1,13 @@
+// in shell
+//sysctl -w vm.overcommit_memory=1
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SIZE (2ULL << 40)
+
+int main() {
+    char* ptr = malloc(SIZE * sizeof(char));
+    getchar();
+    exit(EXIT_SUCCESS);
+}
